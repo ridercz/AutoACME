88a73eb9c6a0457359531ef488089acc73f32473
Michal Altair Valasek <michal.valasek@altairis.cz>
2017-02-15 00:21:44 +0100

    Bumped version

--- Altairis.AutoAcme.Manager/Properties/AssemblyInfo.cs
+++ Altairis.AutoAcme.Manager/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyCompany("Alairis, s. r. o.")]
 [assembly: AssemblyProduct("Altairis AutoACME")]
 [assembly: AssemblyCopyright("Copyright © Altairis, 2017")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
