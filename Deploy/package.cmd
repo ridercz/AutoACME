a62d2e4ff99f44cd9478438eacc28e07f457a915
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-08-16 13:24:26 +0200

    Added signing of assemblies to package script

--- Deploy/package.cmd
+++ Deploy/package.cmd
@@ -1,5 +1,7 @@
 @ECHO OFF
 
+SET SIGNTOOL="C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe"
+
 IF NOT EXIST "C:\Program Files\7-Zip\7z.exe" (
 	ECHO This program requires 7-Zip installed in default path C:\Program Files\7-Zip\7z.exe
 	ECHO You may get it at http://www.7-zip.org/
@@ -27,6 +29,12 @@ ECHO Copying Altairis.AutoAcme.IisSync files...
 COPY /Y ..\Altairis.AutoAcme.IisSync\bin\Debug\*.dll Distribution\lib
 COPY /Y ..\Altairis.AutoAcme.IisSync\bin\Debug\aasync.exe Distribution
 COPY /Y ..\Altairis.AutoAcme.IisSync\bin\Debug\aasync.exe.config Distribution
+
+ECHO Digitally signing EXE files...
+%SIGNTOOL% sign /n "Altairis, s. r. o." /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 "Distribution\autoacme.exe"
+%SIGNTOOL% sign /n "Altairis, s. r. o." /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 "Distribution\aasync.exe"
+%SIGNTOOL% sign /n "Altairis, s. r. o." /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 "Distribution\lib\Altairis.AutoAcme.Core.dll"
+%SIGNTOOL% sign /n "Altairis, s. r. o." /fd SHA256 /tr http://timestamp.digicert.com /td SHA256 "Distribution\lib\Altairis.AutoAcme.Configuration.dll"
 
 ECHO Making ZIP file...
 CD Distribution
