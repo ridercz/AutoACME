f24b075fe492c036b02671bce6d38fef3f064c26
Michal Altair Valášek <michal.valasek@altairis.cz>
2018-02-10 18:43:22 +0100

    Updated version to 1.5.2

--- Altairis.AutoAcme.Configuration/Properties/AssemblyInfo.cs
+++ Altairis.AutoAcme.Configuration/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@
 [assembly: AssemblyDescription("Configuration store for ACME certificate management")]
 [assembly: AssemblyCompany("Alairis, s. r. o.")]
 [assembly: AssemblyProduct("Altairis AutoACME")]
-[assembly: AssemblyCopyright("Copyright © Altairis, 2017")]
-[assembly: AssemblyVersion("1.5.1.0")]
-[assembly: AssemblyFileVersion("1.5.1.0")]
+[assembly: AssemblyCopyright("Copyright © Altairis, 2017-2018
+[assembly: AssemblyVersion("1.5.2.0")]
+[assembly: AssemblyFileVersion("1.5.2.0")]
