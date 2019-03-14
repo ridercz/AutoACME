fc58086a45092e6d7037c9457773cff3294e686d
Michal Altair Valášek <michal.valasek@altairis.cz>
2019-03-14 12:16:51 +0100

    Bumped version to 1.6.2

--- Altairis.AutoAcme.IisSync/Properties/AssemblyInfo.cs
+++ Altairis.AutoAcme.IisSync/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyCompany("Alairis, s. r. o.")]
 [assembly: AssemblyProduct("Altairis AutoACME")]
 [assembly: AssemblyCopyright("Copyright © Michal A. Valášek - Altairis and contributors, 2017-2019")]
-[assembly: AssemblyVersion("1.6.1.0")]
-[assembly: AssemblyFileVersion("1.6.1.0")]
+[assembly: AssemblyVersion("1.6.2.0")]
+[assembly: AssemblyFileVersion("1.6.2.0")]
